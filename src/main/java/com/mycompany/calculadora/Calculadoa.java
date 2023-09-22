30facfec21174fad367521cc1a645b62004daed1
Joao V. koch <joao.koch@duque.g12.br>
2023-09-21 21:14:07 -0300

    criação da tela da calculadora

---
+++ src/main/java/com/mycompany/calculadora/Calculadoa.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculadora;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author 08142
+ */
+public class Calculadoa {
+    public static void main(String[] args) {
+        FrmCalculadora calc = new FrmCalculadora();
+        calc.setTitle("calc dos cria");
+        calc.setBackground(Color.blue);
+        calc.setLocationRelativeTo(calc);
+        calc.setVisible(true);
+    }
+}
